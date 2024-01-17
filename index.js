556ac26f79a774b1dd70ccc302badb6f97cbb80e
paige <88659700+paishee@users.noreply.github.com>
2024-01-17 11:17:22 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const aepl = require('aepl')
+import('node-fetch');
+
+
+aepl.init("FetchedFile", class {
+    constructor(data) {
+        const payload = data.payload
+        const blob = payload.blob
+        
+        this.name = blob.displayName
+        this.url = blob.displayUrl
+        this.type = blob.language
+
+        this.code = blob.rawLines.join("\n")
+        this.lines = blob.rawLines
+
+        this.path = payload.path
+        this.tree = payload.fileTree
+        this.repo = payload.repo
+
+        this.payload = payload
+        this.blob = blob
+        this.data = data
+    }
+});
+
+
+async function fetchFile(link) {
+	let raw = await fetch(link);	
+	let result = await raw.json();
+
+    return new FetchedFile(result)
+}
+
+
+module.exports = fetchFile;
